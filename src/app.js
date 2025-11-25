e69740a8ffbe03491c86b2ee25b8cd39488402ae
kalyanram361 <165807327+kalyanram361@users.noreply.github.com>
2025-11-25 22:51:20 +0530

    aa

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const app=express();
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        origin: process.env.CLIENT_URL || "https://ecomap-1.onrender.com",
         credentials:true,
         methods:["GET","POST","PUT","DELETE","PATCH"],
         allowedHeaders:["Content-Type","Authorization"],
